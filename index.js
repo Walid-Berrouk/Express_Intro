b72700159b19dfd62476e85749821b9c8c1ad183
Walid Berrouk <jw_berrouk@esi.dz>
2022-10-07 16:46:45 +0100

    Adding joi implementation

--- index.js
+++ index.js
@@ -39,12 +39,58 @@
 
 //? Level 3 : Files Serving
 
+// const express = require('express')
+// const path = require('path')
+
+// // Init express
+
+// const app = express()
+
+// // File serving
+
+// app.use(express.static('public'))
+// // app.use('/public', express.static(path.join(__dirname, 'public')))
+
+// // Create Route Handlers
+// app.get('/', (req, res) => {
+//     console.log('Hello World !')
+//     res.end("Hello World !")
+// })
+
+// app.get('/home', (req, res) => {
+//     res.writeHead(200, { "Content-Type": "text/html " })
+//     res.end("<h1>Home Page</h1>")
+// })
+
+// // Listen on a port
+
+// const PORT = process.env.PORT || 5000
+
+// app.listen(PORT, () => console.log(`Server Started on port ${PORT}...`))
+
+//? Level 4 : Validation using joi
+
 const express = require('express')
 const path = require('path')
+const Joi = require('joi');
 
 // Init express
 
 const app = express()
+
+// Data Schema
+const schema = Joi.object({
+    title: Joi.string()
+        .alphanum()
+        .min(3)
+        .max(30)
+        .required(),
+
+    birth_year: Joi.number()
+        .integer()
+        .min(1900)
+        .max(2013),
+})
 
 // File serving
 
@@ -62,13 +108,22 @@ app.get('/home', (req, res) => {
     res.end("<h1>Home Page</h1>")
 })
 
+app.post('/profile', (req, res) => {
+    const profile = req.query
+
+    const result = schema.validate({
+        title: profile.title,
+        birth_year: parseInt(profile.birth_year)
+    })
+
+    res.end(JSON.stringify(result))
+})
+
 // Listen on a port
 
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server Started on port ${PORT}...`))
-
-//? Level 4 : Validation using joi
 
 //? Level 5 : Logging Data
 
