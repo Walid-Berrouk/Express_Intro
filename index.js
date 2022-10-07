01e6adb21731d9a825b771fdc1390771bfefee3d
Walid Berrouk <jw_berrouk@esi.dz>
2022-10-07 13:58:29 +0100

    Level 3 : Static File Serving

--- index.js
+++ index.js
@@ -14,14 +14,44 @@
 
 //? Level 2 : Basic Routing
 
+// const express = require('express')
+
+// // Init express
+
+// const app = express()
+
+// // Create Route Handlers
+
+// app.get('/', (req, res) => {
+//     console.log('Hello World !')
+//     res.end("Hello World !")
+// })
+
+// app.get('/home', (req, res) => {
+//     res.writeHead(200, { "Content-Type": "text/html " })
+//     res.end("<h1>Home Page</h1>")
+// })
+// // Listen on a port
+
+// const PORT = process.env.PORT || 5000
+
+// app.listen(PORT, () => console.log(`Server Started on port ${PORT}...`))
+
+//? Level 3 : Files Serving
+
 const express = require('express')
+const path = require('path')
 
 // Init express
 
 const app = express()
 
-// Create Route Handlers
+// File serving
 
+app.use(express.static('public'))
+// app.use('/public', express.static(path.join(__dirname, 'public')))
+
+// Create Route Handlers
 app.get('/', (req, res) => {
     console.log('Hello World !')
     res.end("Hello World !")
@@ -31,9 +61,17 @@ app.get('/home', (req, res) => {
     res.writeHead(200, { "Content-Type": "text/html " })
     res.end("<h1>Home Page</h1>")
 })
+
 // Listen on a port
 
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server Started on port ${PORT}...`))
 
+//? Level 4 : Validation using joi
+
+//? Level 5 : Logging Data
+
+//? Level 6 : Logging Data using Morgan
+
+//? Level 7 : Authentification using jwt
