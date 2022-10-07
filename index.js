c34db3f471607e73ea4f932211989087cb35044a
Walid Berrouk <jw_berrouk@esi.dz>
2022-10-07 13:57:15 +0100

    Level 2 : Basic Routing

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 // ? Level 1 : Basic Server
 
+// const express = require('express')
+
+// // Init express
+
+// const app = express()
+
+// // Listen on a port
+
+// const PORT = process.env.PORT || 5000
+
+// app.listen(PORT, () => console.log(`Server Started on port ${PORT}...`))
+
+//? Level 2 : Basic Routing
+
 const express = require('express')
 
 // Init express
 
 const app = express()
 
+// Create Route Handlers
+
+app.get('/', (req, res) => {
+    console.log('Hello World !')
+    res.end("Hello World !")
+})
+
+app.get('/home', (req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html " })
+    res.end("<h1>Home Page</h1>")
+})
 // Listen on a port
 
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server Started on port ${PORT}...`))
+
