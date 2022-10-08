7f9f51b5d8c783b6c7f6745dfc4010690576e6da
Walid Berrouk <jw_berrouk@esi.dz>
2022-10-08 15:12:38 +0100

    Level 6 : Logging Data using Morgan

--- index.js
+++ index.js
@@ -127,17 +127,58 @@
 
 //? Level 5 : Logging Data
 
+// const express = require('express')
+// const path = require('path')
+// const myLogger = require('./middlewares/logger')
+
+// // Init express
+
+// const app = express()
+
+// // Logger 
+
+// app.use(myLogger)
+
+// // File serving
+
+// app.use(express.static('public'))
+// // app.use('/public', express.static(path.join(__dirname, 'public')))
+
+// // Create Route Handlers
+// app.get('/', (req, res) => {
+//     console.log('Hello World !')
+//     res.end("Hello World !")
+// })
+
+// app.get('/home', (req, res) => {
+//     res.writeHead(200, { "Content-Type": "text/html " })
+//     res.end("<h1>Home Page</h1>")
+// })
+
+// // Listen on a port
+
+// const PORT = process.env.PORT || 5000
+
+// app.listen(PORT, () => console.log(`Server Started on port ${PORT}...`))
+
+
+//? Level 6 : Logging Data using Morgan
+
 const express = require('express')
 const path = require('path')
-const myLogger = require('./middlewares/logger')
+const Logger = require("morgan")
+const fs = require('fs')
 
 // Init express
 
 const app = express()
 
+// create a write stream (in append mode)
+var accessLogStream = fs.createWriteStream(path.join(__dirname, "storage", "logsMorgan.txt"), { flags: 'a' })
+ 
 // Logger 
+app.use(Logger('combined', { stream: accessLogStream }))
 
-app.use(myLogger)
 
 // File serving
 
@@ -160,8 +201,5 @@ app.get('/home', (req, res) => {
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server Started on port ${PORT}...`))
-
-
-//? Level 6 : Logging Data using Morgan
 
 //? Level 7 : Authentification using jwt
